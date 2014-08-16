0831be443529ab9d6891b1f48edb45c58654a12d
Karen Etheridge <ether@cpan.org>
2014-08-16 16:37:29 -0700

    more keywords

--- lib/Dist/Zilla/Plugin/Test/CheckBreaks.pm
+++ lib/Dist/Zilla/Plugin/Test/CheckBreaks.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::Test::CheckBreaks;
 # ABSTRACT: Generate a test that shows what modules you are breaking
-# KEYWORDS: prerequisites upstream dependencies modules conflicts breaks breakages metadata
+# KEYWORDS: distribution prerequisites upstream dependencies modules conflicts breaks breakages metadata
 # vim: set ts=8 sw=4 tw=78 et :
 
 use Moose;
