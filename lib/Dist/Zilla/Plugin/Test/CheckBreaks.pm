5670788a7cb806dc1a91ced7eec3964bf16874b4
Karen Etheridge <ether@cpan.org>
2016-01-26 18:45:15 -0800

    increment $VERSION after 0.015 release

--- lib/Dist/Zilla/Plugin/Test/CheckBreaks.pm
+++ lib/Dist/Zilla/Plugin/Test/CheckBreaks.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::Test::CheckBreaks;
 # KEYWORDS: distribution prerequisites upstream dependencies modules conflicts breaks breakages metadata
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '0.015';
+our $VERSION = '0.016';
 
 use Moose;
 with (
